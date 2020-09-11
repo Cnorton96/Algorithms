8b8bb06110c9c61f60432c4d741a5047f9697bdf
Christian Norton <Christiannorton350@gmail.com>
2020-09-11 16:19:23 -0500

    Created project OneEditAway - September 11, 2020 PalindromePermutation - september 11, 2020

---
+++ src/PalindromePermutation.java
@@ -0,0 +1,40 @@
+public class PalindromePermutation {
+    /* Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase
+       that is the same backwards and forwards. A permutation is a rearrangement of letters.
+     */
+
+    public static boolean isPermutation(String str){
+        int odd = 0;
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        for(char c : str.toCharArray()){
+            int x = getCharNumber(c);
+            if( x != -1){
+                if(table[x] % 2 == 1){
+                    odd++;
+                }else{
+                    odd--;
+                }
+            }
+        }
+        return odd <= 1;
+
+    }
+
+    public static int getCharNumber(char c){
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        if(a <= val && val <= z){
+            return val - a;
+        }
+        return -1;
+    }
+
+
+
+    public static void main(String[] args){
+        String str = "racecar";
+        System.out.println("Is the word racecar a palindrome of a permutation: " + isPermutation(str));
+
+    }
+}
